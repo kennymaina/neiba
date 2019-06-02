56cb4c33a0df5356bd9d6d2dec60e194950e51f2
kennymaina <victormurigi@outlook.com>
2019-06-02 08:51:12 +0300

    making landing page be viewed

--- Neibas/views.py
+++ Neibas/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+
+
+
 
 # Create your views here.
+
+
+
+# @login_required(login_url="/accounts/login/")
+def home(request):
+    # hoods = Hood.objects.all()
+    
+    return render(request,"home.html",locals())
+
